e1f2157d38788255a76113084f233fe9f56faa17
Ignacy Kasperowicz <fikufik@gmail.com>
2014-01-27 23:18:54 +0100

    Revert: Assets workaround

--- app/extensions/assets.rb
+++ app/extensions/assets.rb
@@ -33,10 +33,9 @@ module Blog
         end
 
         app.configure :production do
-          assets.cache = Sprockets::Cache::FileStore.new('./tmp')
-          # assets.cache          = Sprockets::Cache::MemcacheStore.new
-          # assets.js_compressor  = Closure::Compiler.new
-          # assets.css_compressor = YUI::CssCompressor.new
+          assets.cache          = Sprockets::Cache::MemcacheStore.new
+          assets.js_compressor  = Closure::Compiler.new
+          assets.css_compressor = YUI::CssCompressor.new
         end
 
         app.helpers Helpers
