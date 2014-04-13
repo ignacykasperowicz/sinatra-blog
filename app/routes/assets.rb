239aa88917785ee2481032f967bb242f56177629
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2014-04-13 22:43:46 +0200

    Test PDFkit

--- app/routes/assets.rb
+++ app/routes/assets.rb
@@ -6,7 +6,7 @@ module Blog
         settings.assets.call(env)
       end
 
-      get '/to_pdf' do
+      get '/to_pdf/' do
         html = haml(:pdf)
         kit = PDFKit.new(html)
         kit.to_pdf
