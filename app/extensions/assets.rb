e8fa2bce9d65d29e7f5be1700d8f5b4190d6279d
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2015-05-21 12:06:51 +0200

    Assets compilation fixes

--- app/extensions/assets.rb
+++ app/extensions/assets.rb
@@ -28,17 +28,15 @@ module Blog
 
         app.set :asset_host, ''
 
-        assets.cache = Sprockets::Cache::FileStore.new('./tmp')
+        app.configure :development do
+          assets.cache = Sprockets::Cache::FileStore.new('./tmp')
+        end
 
-        # app.configure :development do
-        #   assets.cache = Sprockets::Cache::FileStore.new('./tmp')
-        # end
-
-        # app.configure :production do
-        #   assets.cache          = Sprockets::Cache::MemcacheStore.new
-        #   assets.js_compressor  = Closure::Compiler.new
-        #   assets.css_compressor = YUI::CssCompressor.new
-        # end
+        app.configure :production do
+          assets.cache          = Sprockets::Cache::MemcacheStore.new
+          assets.js_compressor  = Closure::Compiler.new
+          assets.css_compressor = YUI::CssCompressor.new
+        end
 
         app.helpers Helpers
       end
